f2e142ca14a64bc1b8106843e54cf0893483d8ca
Florian Sylvain <floriansylvainpro@gmail.com>
2023-02-13 22:09:20 +0100

    Implemented everything I guess

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # ESD Paris Digital Event 2023 - Exp Visiteur - API
+
+API for 2023 ESD Paris Digital Event reservations
+
+## Environment variables
+
+```
+API_PORT=
+AIRTABLE_TOKEN=
+AIRTABLE_BASE=
+```
+
+## Usage
+
+Make sure to add env variables in a .env at the project's root.
+
+### Setup
+
+```
+npm install
+```
+
+### Dev
+
+```
+npm run dev
+```
+
+### Production
+
+```
+npm run start
+```
--- app.js
+++ app.js
@@ -4,9 +4,12 @@ import cors from "cors"
 import * as dotenv from "dotenv"
 import fs from "fs"
 import { z } from "zod"
-// import { showsRouter } from "./routes/shows.js"
+import Airtable from "airtable"
 
 dotenv.config()
+
+const base = new Airtable({ apiKey: process.env.AIRTABLE_TOKEN }).base(process.env.AIRTABLE_BASE)
+
 const app = express()
 const port = process.env.API_PORT
 
@@ -15,7 +18,7 @@ app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/v1/", (req, res, next) => {
+app.get("/v1/", (req, res) => {
 	res.send({ message: "ESD Paris Expvisit API v1.0" })
 })
 
@@ -23,40 +26,123 @@ const userDataValidator = z.object({
 	lastname: z.string().max(50),
 	name: z.string().max(50),
 	email: z.string().email(),
-	time: z.string().refine((date) => {
-		return !isNaN(new Date(date).getTime())
-	}),
+	schedules: z.array(
+		z.object({
+			id: z.string(),
+			datetime: z.string().refine((date) => {
+				return !isNaN(new Date(date).getTime())
+			}),
+		})
+	),
 })
+
+const schedulesIdNamesRelation = {
+	drone: "Atelier drone",
+	escapeGame: "Atelier Escape Game",
+}
 
 const schedulesFilePath = "./creneaux.json"
 
-// a refactor
-app.post("/v1/schedules/reserve", (req, res, next) => {
+function getSchedule(schedules, datetime) {
+	if (schedules == undefined) return undefined
+
+	return schedules.find(
+		(schedule) => new Date(schedule.datetime).getTime() === new Date(datetime).getTime()
+	)
+}
+
+function setScheduleAvailability(schedule) {
+	if (!schedule?.available) return false
+	schedule.available = false
+}
+
+function parseValidatedSchedules(schedules) {
+	const parsedSchedules = {}
+	schedules.forEach((schedule) => {
+		parsedSchedules[schedulesIdNamesRelation[schedule.id]] = new Date(schedule.datetime)
+	})
+	return parsedSchedules
+}
+
+async function pushNewEntryToAirtable(userData) {
+	await base("Table 1")
+		.create({
+			Email: userData.email,
+			Nom: userData.lastname,
+			Prenom: userData.name,
+			...parseValidatedSchedules(userData.schedules),
+		})
+		.catch((error) => console.error(error))
+}
+
+function reserveSchedule(scheduleData) {
+	const schedules = JSON.parse(fs.readFileSync(schedulesFilePath))
+
+	const targetSchedule = getSchedule(schedules[scheduleData.id], scheduleData.datetime)
+	if (targetSchedule == undefined) {
+		return {
+			success: false,
+			message: `Schedule ${scheduleData.id} does not exist.`,
+		}
+	}
+
+	const err = setScheduleAvailability(targetSchedule)
+	if (err !== undefined) {
+		return {
+			success: false,
+			message:
+				`Schedule ${new Date(scheduleData.datetime)}` +
+				`(${scheduleData.id}) is not available.`,
+		}
+	}
+
+	fs.writeFileSync(schedulesFilePath, JSON.stringify(schedules, null, 4))
+
+	return { success: true }
+}
+
+app.get("/v1/schedules", async (req, res, next) => {
+	const schedules = JSON.parse(fs.readFileSync(schedulesFilePath))
+	res.send(schedules)
+})
+
+app.post("/v1/schedules/reserve", async (req, res) => {
 	const user = userDataValidator.safeParse(req.body)
 	if (!user.success) {
 		res.status(400).json({ issues: user })
 		return
 	}
 
-	const schedules = JSON.parse(fs.readFileSync(schedulesFilePath))
-	const scheduleIndex = schedules.findIndex(
-		(schedule) =>
-			new Date(schedule.date).getTime() ===
-			new Date(user.data.time).getTime()
-	)
+	const successfullyReservedSchedules = []
+	let finalMessage = "Schedules successfully reserved!"
 
-	if (!schedules[scheduleIndex].available) {
-		res.status(400).json({ message: "Schedule not available anymore." })
+	for (const schedule of user.data.schedules) {
+		const result = reserveSchedule(schedule)
+		if (!result.success) {
+			finalMessage = result.message
+			break
+		}
+		successfullyReservedSchedules.push(schedule)
+	}
+
+	if (successfullyReservedSchedules.length === 0) {
+		res.status(400).send({
+			message: finalMessage,
+		})
 		return
 	}
 
-	schedules[scheduleIndex].available = false
+	await pushNewEntryToAirtable({
+		email: user.data.email,
+		lastname: user.data.lastname,
+		name: user.data.name,
+		schedules: successfullyReservedSchedules,
+	})
 
-	fs.writeFileSync(schedulesFilePath, JSON.stringify(schedules, null, 4))
-
-	res.send({ reservedSchedule: schedules[scheduleIndex] })
+	res.send({
+		message: finalMessage,
+		successfullyReservedSchedules,
+	})
 })
-
-// app.use("/v1/shows", showsRouter)
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
